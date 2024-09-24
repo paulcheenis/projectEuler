bf01f9fa45883d1d3499d2e3ee2913a5c8d1f084
paulcheenis <kdang45@gmail.com>
2024-09-24 14:15:43 -0700

    continued work on problem 9

--- ID_09/special_pythag_triple.py
+++ ID_09/special_pythag_triple.py
@@ -1,16 +1,25 @@
-import math
-
-a = 3
-b = 4
-c = 5
-
-abc_sum = a + b + c
-
-while abc_sum != 1000:
-    try:
-        first_term = a**2 + b**2
-        second_term = c**2
-        abc_sum = a + b + c
-    except ValueError:
+def pythagorean_triple(n):
+    a = 2*n
+    b = n**2 - 1
+    c = n**2 + 1
+    first_term = a**2 + b**2
+    second_term = c**2
+    sum = a + b + c
+    product = a * b * c
+    array = [first_term, second_term, sum, product]
+    return array
         
-        
+# print(pythagorean_triple(21.8665))
+
+def pythagorean_triple2(a, b, c):
+    first_term = a**2 + b**2
+    second_term = c**2
+    sum = a + b + c
+    product = a * b * c
+    array = [first_term, second_term, sum, product]
+    return array
+
+#print(pythagorean_triple2(240,320,400)) #3, 4, 5
+#print(pythagorean_triple2(150, 360, 390)) #5, 12, 13
+#print(pythagorean_triple2(240, 320, 400)) #6, 8, 10
+print(pythagorean_triple2(85, 455, 460))
